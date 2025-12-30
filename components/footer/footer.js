29d97e5472b57d0743eb6c860c85e938d6a7ef52
Binderjr <binderbayas@gmail.com>
2025-12-30 14:14:49 +0900

    Pro

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,2 +1,8 @@
 document.getElementById("footer-year").textContent =
   new Date().getFullYear();
+
+fetch("/components/social-btn/social-btn.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("footer-social").innerHTML = html;
+  });
