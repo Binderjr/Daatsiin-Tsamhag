42e058de0b6176a041065a0de1678f734975a0d0
Binderjr <binderbayas@gmail.com>
2026-01-05 22:52:23 +0900

    sitemap

--- seo-build.js
+++ seo-build.js
@@ -1,5 +1,9 @@
+const fs = require("fs");
+
+const BASE_URL = "https://daatsiintsamkhag.com";
+
 const pages = [
-  "",
+  "/",
   "/about/",
   "/projects/",
   "/newsroom/",
@@ -8,18 +12,16 @@ const pages = [
   "/contact/"
 ];
 
-const BASE_URL = "https://daatsiintsamkhag.com";
-
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages.map(p => `
   <url>
     <loc>${BASE_URL}${p}</loc>
     <changefreq>weekly</changefreq>
-    <priority>${p === "" ? "1.0" : "0.8"}</priority>
+    <priority>${p === "/" ? "1.0" : "0.8"}</priority>
   </url>
 `).join("")}
-</urlset>
-`;
+</urlset>`;
 
-console.log(sitemap);
+fs.writeFileSync("sitemap.xml", sitemap);
+console.log("sitemap.xml generated");
